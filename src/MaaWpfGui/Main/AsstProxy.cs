66657038f17303a8cc3b67dc407a830f158b7605
uye <99072975+ABA2396@users.noreply.github.com>
2025-06-03 20:24:15 +0800

    feat: 或许我应该换一份作业？

--- src/MaaWpfGui/Main/AsstProxy.cs
+++ src/MaaWpfGui/Main/AsstProxy.cs
@@ -1110,6 +1110,11 @@ namespace MaaWpfGui.Main
                             {
                                 Instances.CopilotViewModel.AddLog(LocalizationHelper.GetString("MissingOperators"), UiLogColor.Error);
                             }
+
+                            if (missingOpers is not null && missingOpers.Count >= 2)
+                            {
+                                AchievementTrackerHelper.Instance.Unlock(AchievementIds.Irreplaceable);
+                            }
                         }
 
                         break;
